d33b57b600cbe0e2c16aa2f4fa9ba1092b3210c0
Mahesh <Mahesh.lkp1234@gmail.com>
2021-09-28 23:57:13 -0500

    added card changes

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Card () {
+    return (
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="Robots" src="https://www.robohash.org/test?200*250"></img>
+            <div>
+                <h2>Mahesh Lakkapally</h2>
+                <p>Mahesh.lkp@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
